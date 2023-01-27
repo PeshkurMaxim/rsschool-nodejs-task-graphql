7d4fcc9951c1abd9198300dd67ed082ebb77ba06
Пешкур Максим <m.peshkur@ostrovchistoty.by>
2023-01-27 10:27:28 +0300

    add rest api posts

--- src/routes/posts/index.ts
+++ src/routes/posts/index.ts
@@ -1,53 +1,71 @@
 import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
-// import { idParamSchema } from '../../utils/reusedSchemas';
-// import { createPostBodySchema, changePostBodySchema } from './schema';
-// import type { PostEntity } from '../../utils/DB/entities/DBPosts';
+import { idParamSchema } from '../../utils/reusedSchemas';
+import { createPostBodySchema, changePostBodySchema } from './schema';
+import type { PostEntity } from '../../utils/DB/entities/DBPosts';
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
   fastify
 ): Promise<void> => {
-  // fastify.get('/', async function (request, reply): Promise<PostEntity[]> {});
+  fastify.get(
+    '/', 
+    async function (request, reply): Promise<PostEntity[]> {
+      return fastify.db.posts.findMany();
+    }
+  );
 
-  // fastify.get(
-  //   '/:id',
-  //   {
-  //     schema: {
-  //       params: idParamSchema,
-  //     },
-  //   },
-  //   async function (request, reply): Promise<PostEntity> {}
-  // );
+  fastify.get(
+    '/:id',
+    {
+      schema: {
+        params: idParamSchema,
+      },
+    },
+    async function (request, reply): Promise<PostEntity> {
+      const post = await fastify.db.posts.findOne({key: "id", equals: request.params.id});
 
-  // fastify.post(
-  //   '/',
-  //   {
-  //     schema: {
-  //       body: createPostBodySchema,
-  //     },
-  //   },
-  //   async function (request, reply): Promise<PostEntity> {}
-  // );
+      if (!post)
+        throw fastify.httpErrors.badRequest('post not found');
 
-  // fastify.delete(
-  //   '/:id',
-  //   {
-  //     schema: {
-  //       params: idParamSchema,
-  //     },
-  //   },
-  //   async function (request, reply): Promise<PostEntity> {}
-  // );
+      return post;
+    }
+  );
 
-  // fastify.patch(
-  //   '/:id',
-  //   {
-  //     schema: {
-  //       body: changePostBodySchema,
-  //       params: idParamSchema,
-  //     },
-  //   },
-  //   async function (request, reply): Promise<PostEntity> {}
-  // );
+  fastify.post(
+    '/',
+    {
+      schema: {
+        body: createPostBodySchema,
+      },
+    },
+    async function (request, reply): Promise<PostEntity> {
+      return fastify.db.posts.create(request.body);
+    }
+  );
+
+  fastify.delete(
+    '/:id',
+    {
+      schema: {
+        params: idParamSchema,
+      },
+    },
+    async function (request, reply): Promise<PostEntity> {
+      return fastify.db.posts.delete(request.params.id);
+    }
+  );
+
+  fastify.patch(
+    '/:id',
+    {
+      schema: {
+        body: changePostBodySchema,
+        params: idParamSchema,
+      },
+    },
+    async function (request, reply): Promise<PostEntity> {
+      return fastify.db.posts.change(request.params.id, request.body);
+    }
+  );
 };
 
 export default plugin;
